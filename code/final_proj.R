e40afcc8cc55fbdaf8cc4b77126af6ef73aadd67
Aravind Mantravadi <aravind1338@gmail.com>
2020-12-09 18:01:27 -0500

    Added the results code and table

--- code/final_proj.R
+++ code/final_proj.R
@@ -5,7 +5,7 @@
 ## intake primarily at home or from elsewhere?
 ##
 ## Author(s): Aravind Mantravadi, amantrav@umich.edu
-## Updated: December 6th, 2020
+## Updated: December 9th, 2020
 
 # libraries: ------------------------------------------------------------------
 
@@ -27,12 +27,13 @@ demographics =
 nutrition = 
   nutrition %>% drop_na() %>% select(seqn, wtdrd1, wtdr2d, dr1iprot, 
                        dr1icarb, dr1isugr, dr1ifibe, dr1itfat, dr1fs) %>%
-  rename(id = seqn, weight_3 = wtdrd1, weight_4 = wtdr2d, proteins = dr1iprot,
-         carbohydrates = dr1icarb, sugars = dr1isugr, fibers = dr1ifibe, 
-         fats = dr1itfat, source = dr1fs) %>% 
-  mutate(source = if_else(source == 1 | source == 10 | source == 11 
-                          | source == 12 | source == 18 
-                          | source == 19 | source == 20, 
+  rename(id = seqn, weight_3 = wtdrd1, weight_4 = wtdr2d, 
+         est_proteins = dr1iprot, est_carbohydrates = dr1icarb, 
+         est_sugars = dr1isugr, est_fibers = dr1ifibe, 
+         est_fats = dr1itfat, Source = dr1fs) %>% 
+  mutate(Source = if_else(Source == 1 | Source == 10 | Source == 11 
+                          | Source == 12 | Source == 18 
+                          | Source == 19 | Source == 20, 
                           "home-cooked", "outside", "missing"))
 
 
@@ -51,35 +52,41 @@ dataset = merge(nutrition, demographics, "id")
 des = svydesign(ids = ~0, weights = ~weight_1 + weight_2 + weight_3 + weight_4, 
                 data = dataset)
 
-means = svyby(~proteins + carbohydrates + sugars + fibers + fats, 
-              by = ~source, des, svymean)
+means = svyby(~est_proteins + est_carbohydrates + est_sugars + 
+                est_fibers + est_fats, by = ~Source, des, svymean)
 
 
 statistic = qnorm(0.975)
 # Create the confidence interval columns for each macronutrient
 
-means = means %>% mutate(lower.proteins = proteins - se.proteins*statistic,
-                             upper.proteins = proteins + se.proteins*statistic, 
-                         lower.carbohydrates = carbohydrates - se.carbohydrates*statistic, 
-                         upper.carbohydrates = carbohydrates + se.carbohydrates*statistic,
-                         lower.sugars = sugars - se.sugars*statistic, 
-                         upper.sugars = sugars + se.sugars*statistic,
-                         lower.fibers = fibers - se.fibers*statistic, 
-                         upper.fibers = fibers + se.fibers*statistic,
-                         lower.fats = fats - se.fats*statistic, 
-                         upper.fats = fats + se.fats*statistic)
+means = means %>% 
+  mutate(l.p = est_proteins - se.est_proteins*statistic,
+         u.p = est_proteins + se.est_proteins*statistic,
+         l.c = est_carbohydrates - se.est_carbohydrates*statistic,
+         u.c = est_carbohydrates + se.est_carbohydrates*statistic, 
+         l.s = est_sugars - se.est_sugars*statistic, 
+         u.s = est_sugars + se.est_sugars*statistic, 
+         l.f = est_fibers - se.est_fibers*statistic, 
+         u.f = est_fibers + se.est_fibers*statistic, 
+         l.fa = est_fats - se.est_fats*statistic, 
+         u.fa = est_fats + se.est_fats*statistic,
+         Proteins = sprintf('%4.1f (%4.1f, %4.1f)', est_proteins, l.p, u.p), 
+         Carbohydrates = sprintf('%4.1f (%4.1f, %4.1f)', 
+                                 est_carbohydrates, l.c, u.c), 
+         Sugars = sprintf('%4.1f (%4.1f, %4.1f)', est_sugars, l.s, u.s), 
+         Fibers = sprintf('%4.1f (%4.1f, %4.1f)', est_fibers, l.f, u.f), 
+         Fats = sprintf('%4.1f (%4.1f, %4.1f)', est_fats, l.fa, u.fa)) %>%
+  select(Source, Proteins, Carbohydrates, Sugars, Fibers, Fats)
 
 
 
-means %>%
-  ggplot(aes_string(x = "proteins", y = "group", color = "source")) +
-  geom_point(position = position_dodge(width = 0.4)) +
-  geom_errorbarh(aes_string(xmin = "lower.proteins", xmax = "upper.proteins"),
-                 position = position_dodge(width = 0.4)) +
-  labs(x = "proteins", y = "") + 
-  scale_color_manual(values = c("limegreen", "skyblue"))
-
-
+#means %>%
+#  ggplot(aes_string(x = "proteins", y = "group", color = "source")) +
+#  geom_point(position = position_dodge(width = 0.4)) +
+#  geom_errorbarh(aes_string(xmin = "lower.proteins", xmax = "upper.proteins"),
+#                 position = position_dodge(width = 0.4)) +
+#  labs(x = "proteins", y = "") + 
+#  scale_color_manual(values = c("limegreen", "skyblue"))
 
 
 
